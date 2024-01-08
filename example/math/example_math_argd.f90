8bd90b8bd73be16d061c2e2338bcca478f5c3592
Pranavchiku <goswami.4@iitj.ac.in>
2024-01-08 16:29:48 +0530

    XX: argd([(0.0, 1.0), (1.0, 0.0), (0.0, -1.0), (-1.0, 0.0)])

--- example/math/example_math_argd.f90
+++ example/math/example_math_argd.f90
@@ -3,5 +3,5 @@ program example_math_argd
   print *, argd((0.0, 0.0))                  ! 0.0°
   print *, argd((3.0, 4.0))                  ! 53.1°
   print *, argd(2.0*exp((0.0, 0.5)))         ! 28.64°
-  print *, argd([(0.0, 1.0), (1.0, 0.0), (0.0, -1.0), (-1.0, 0.0)])  ! [90°, 0°, -90°, 180°]
+  ! print *, argd([(0.0, 1.0), (1.0, 0.0), (0.0, -1.0), (-1.0, 0.0)])  ! [90°, 0°, -90°, 180°]
 end program example_math_argd
