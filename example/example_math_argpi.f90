3ac977c41ad7610626090bda02607ba3a8a97882
Pranavchiku <85227306+Pranavchiku@users.noreply.github.com>
2024-01-23 09:03:24 +0000

    Deploying to stdlib-fpm from @ Pranavchiku/stdlib-fortran-lang@dffafa06df82619a9c180d28dc5b73a02864dc38 🚀

--- example/example_math_argpi.f90
+++ example/example_math_argpi.f90
@@ -3,5 +3,5 @@ program example_math_argpi
   print *, argpi((0.0, 0.0))                  ! 0.0
   print *, argpi((3.0, 4.0))                  ! 0.295
   print *, argpi(2.0*exp((0.0, 0.5)))         ! 0.159
-  ! print *, argpi([(0.0, 1.0), (1.0, 0.0), (0.0, -1.0), (-1.0, 0.0)])  ! [0.5, 0.0, -0.5, 1.0]
+  print *, argpi([(0.0, 1.0), (1.0, 0.0), (0.0, -1.0), (-1.0, 0.0)])  ! [0.5, 0.0, -0.5, 1.0]
 end program example_math_argpi
