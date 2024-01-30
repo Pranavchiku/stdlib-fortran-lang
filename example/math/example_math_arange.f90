6c1e569f9aa0c685b6c6b330713816e9b455888d
Pranavchiku <goswami.4@iitj.ac.in>
2024-01-30 13:51:48 +0530

    XX: cmplx(1.0, 1.0)

--- example/math/example_math_arange.f90
+++ example/math/example_math_arange.f90
@@ -11,7 +11,7 @@ program example_math_arange
   print *, arange(0.0, 5.0)           ! [0.0,1.0,2.0,3.0,4.0,5.0]
   print *, arange(0.0, 6.0, 2.5)       ! [0.0,2.5,5.0]
 
-  print *, (1.0, 1.0)*arange(3)       ! [(1.0,1.0),(2.0,2.0),[3.0,3.0]]
+  ! print *, cmplx(1.0, 1.0)*arange(3)       ! [(1.0,1.0),(2.0,2.0),[3.0,3.0]]
 
   print *, arange(0.0, 2.0, -2.0)      ! [0.0,2.0].     Not recommended: `step` argument is negative!
   print *, arange(0.0, 2.0, 0.0)       ! [0.0,1.0,2.0]. Not recommended: `step` argument is zero!
