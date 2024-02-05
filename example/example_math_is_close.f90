744cad40eeb7899a3db2b66b50d3c40d4ab7bd53
Pranavchiku <85227306+Pranavchiku@users.noreply.github.com>
2024-02-05 05:17:08 +0000

    Deploying to stdlib-fpm from @ Pranavchiku/stdlib-fortran-lang@ab7eedfb30eb98552a68038fa9d2ec04b64b961e 🚀

--- example/example_math_is_close.f90
+++ example/example_math_is_close.f90
@@ -2,13 +2,25 @@ program example_math_is_close
 
   use stdlib_math, only: is_close
   real :: x(2) = [1, 2], y, NAN
+  logical :: result(2)
 
   y = -3
   NAN = sqrt(y)
 
   print *, is_close(x, [real :: 1, 2.1])       ! [T, F]
+  result = is_close(x, [real :: 1, 2.1])
+  if (.not. result(1)) error stop
+  if (result(2)) error stop
+
   print *, is_close(2.0, 2.1, abs_tol=0.1)    ! T
+  if (.not. is_close(2.0, 2.1, abs_tol=0.1)) error stop
   print *, NAN, is_close(2.0, NAN), is_close(2.0, NAN, equal_nan=.true.)   ! NAN, F, F
+
+  if (is_close(NAN, NAN)) error stop
+  ! if (is_close(NAN, NAN, equal_nan=.true.)) error stop ! TODO: fix this
+
   print *, is_close(NAN, NAN), is_close(NAN, NAN, equal_nan=.true.)        ! F, T
+  if (is_close(NAN, NAN)) error stop
+  if (.not. is_close(NAN, NAN, equal_nan=.true.)) error stop
 
 end program example_math_is_close
