83794fb2fc4a63ce1b79127fa55a7cb9ca418358
pranavchiku <goswami.4@iitj.ac.in>
2024-02-09 21:01:56 +0530

    example: add error stop tests

--- example/math/example_logspace_complex.f90
+++ example/math/example_logspace_complex.f90
@@ -9,4 +9,8 @@ program example_logspace_complex
   complex(dp) :: z(11) ! Complex values raised to complex powers results in complex values
 
   z = logspace(start, end, 11)
+  print *, "z = ", z
+  print *, abs(sum(z))
+
+  if (abs(abs(sum(z)) - 9.93335211079602432e+09_dp) > 1e-12) error stop
 end program example_logspace_complex
--- example/math/example_math_arg.f90
+++ example/math/example_math_arg.f90
@@ -1,7 +1,28 @@
 program example_math_arg
   use stdlib_math, only: arg
+  real :: res(4)
+  integer :: i
+  complex :: z(4)
   print *, arg((0.0, 0.0))                  ! 0.0
+  if (abs(arg((0.0, 0.0)) - 0.0) > 1.0e-8) error stop
+
   print *, arg((3.0, 4.0))                  ! 0.927
+  if (abs(arg((3.0, 4.0)) - 0.9272952180016122) > 1.0e-8) error stop
+
   print *, arg(2.0*exp((0.0, 0.5)))         ! 0.5
+  if (abs(arg(2.0*exp((0.0, 0.5))) - 0.5) > 1.0e-8) error stop
+
   print *, arg([(0.0, 1.0), (1.0, 0.0), (0.0, -1.0), (-1.0, 0.0)])  ! [π/2, 0.0, -π/2, π]
+
+  z = [(0.0, 1.0), (1.0, 0.0), (0.0, -1.0), (-1.0, 0.0)]
+  do i = 1, 4
+    res(i) = arg(z(i))
+    print *, res(i)
+  end do
+
+  if (abs(res(1) - 1.57079637) > 1.0e-8) error stop
+  if (abs(res(2) - 0.0) > 1.0e-8) error stop
+  if (abs(res(3) + 1.57079637) > 1.0e-8) error stop
+  if (abs(res(4) - 3.1415927) > 1.0e-8) error stop
+
 end program example_math_arg
