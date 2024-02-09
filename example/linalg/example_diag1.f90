10b647d4e7c54c6c3a0a188393ca50bafcf58b36
Pranavchiku <goswami.4@iitj.ac.in>
2024-02-09 18:15:30 +0530

    XX: allocate(A(10, 10))

--- example/linalg/example_diag1.f90
+++ example/linalg/example_diag1.f90
@@ -3,5 +3,6 @@ program example_diag1
   implicit none
   real, allocatable :: A(:, :)
   integer :: i
+  allocate(A(10, 10))
   A = diag([(1, i=1, 10)]) ! creates a 10 by 10 identity matrix
 end program example_diag1
