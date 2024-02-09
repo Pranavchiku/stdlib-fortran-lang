935d2d6485a8af5c70b6635e8324fdea59249999
Pranavchiku <goswami.4@iitj.ac.in>
2024-02-09 18:59:09 +0530

    example error stop test

--- example/linalg/example_diag1.f90
+++ example/linalg/example_diag1.f90
@@ -4,4 +4,8 @@ program example_diag1
   real, allocatable :: A(:, :)
   integer :: i
   A = diag([(1, i=1, 10)]) ! creates a 10 by 10 identity matrix
+  print *, A
+  print *, "sum(A) = ", sum(A)
+  ! if (abs(sum(A) - 9.0) > 1e-8) error stop ! TODO: fix this
+  if (any(.not. (abs(A - 1.0) > 1e-8 .or. abs(A - 0.0) > 1e-8))) error stop
 end program example_diag1
