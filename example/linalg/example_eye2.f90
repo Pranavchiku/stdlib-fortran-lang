0498034c60df46ce5abc54c23b314c7337e8cfc1
Pranavchiku <goswami.4@iitj.ac.in>
2024-02-09 18:23:43 +0530

    XX: all workaround

--- example/linalg/example_eye2.f90
+++ example/linalg/example_eye2.f90
@@ -1,5 +1,12 @@
 program example_eye2
   use stdlib_linalg, only: eye, diag
   implicit none
-  print *, all(eye(4) == diag([1, 1, 1, 1])) ! prints .true.
+  ! print *, all(eye(4) == diag([1, 1, 1, 1])) ! prints .true. ! TODO: Fix this
+  real :: A(4, 4)
+  real :: B(4, 4)
+  A = eye(4)
+  print *, A
+  B = diag([1, 1, 1, 1])
+  print *, B
+  print *, all(A == B) ! prints .true.
 end program example_eye2
