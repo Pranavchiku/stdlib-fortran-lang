c532db23548329e008e3a8dd48146d657f658dae
HarshilShah1804 <hkshah1978@gmail.com>
2025-06-13 01:11:16 +0530

    XX: add the iostat and iomsg arguments explicitely in read/write

--- example/string_type/example_fwrite.f90
+++ example/string_type/example_fwrite.f90
@@ -2,15 +2,16 @@ program example_fwrite
   use stdlib_string_type
   implicit none
   type(string_type) :: string
-  integer :: io
+  integer :: io, iostat
+  character(len=100) :: iomsg
   string = "Important saved value"
 
   open (newunit=io, form="formatted", status="scratch")
-  write (io, *) string
-  write (io, *)
+  write (io, *, iostat=iostat, iomsg=iomsg) string
+  write (io, *, iostat=iostat, iomsg=iomsg)
 
   rewind (io)
 
-  read (io, *) string
+  read (io, *, iostat=iostat, iomsg=iomsg) string
   close (io)
 end program example_fwrite
