6c813f32ccee6937dfbbbcdbf8a6d48559785f12
HarshilShah1804 <hkshah1978@gmail.com>
2025-06-12 23:52:06 +0530

    XX: make direct function call to constructor

--- example/string_type/example_constructor_integer.f90
+++ example/string_type/example_constructor_integer.f90
@@ -2,8 +2,8 @@ program example_constructor_integer
   use stdlib_string_type
   implicit none
   type(string_type) :: string
-  string = string_type(42)
+  string = construct_string_type(42)
 ! len(string) == 2
-  string = string_type(-289)
+  string = construct_string_type(-289)
 ! len(string) == 4
 end program example_constructor_integer
