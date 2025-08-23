e820425b9479148e7760d54d9340da8c20c0ccfe
AnshMehta1 <ansh.mehta522007@gmail.com>
2025-08-24 00:57:51 +0530

    Revert "XX: [hash_functions] submodule issue"
    This reverts commit a8c007617382fffcb4af49c42798030074a18bec.

--- test/hash_functions_perf/test_32_bit_hash_performance.f90
+++ test/hash_functions_perf/test_32_bit_hash_performance.f90
@@ -9,9 +9,6 @@ program test_32_bit_hash_performance
         int64
 
     use stdlib_hash_32bit
-    use stdlib_hash_32bit_fnv
-    use stdlib_hash_32bit_water
-    use stdlib_hash_32bit_nm
 
     implicit none
 
@@ -70,9 +67,9 @@ contains
 
     subroutine test_fnv_1()
         integer :: index2
-        integer(int32) :: hash
+        integer(int_hash) :: hash
         real :: t1, t2, tdiff
-        integer(int32) :: summary(repeat)
+        integer(int_hash) :: summary(repeat)
 
         do k=1, size(block_size)
             call cpu_time(t1)
@@ -94,9 +91,9 @@ contains
 
     subroutine test_fnv_1a()
         integer :: index2
-        integer(int32) :: hash
+        integer(int_hash) :: hash
         real :: t1, t2, tdiff
-        integer(int32) :: summary(repeat)
+        integer(int_hash) :: summary(repeat)
 
         do k=1, size(block_size)
             call cpu_time(t1)
@@ -118,10 +115,10 @@ contains
 
     subroutine test_nmhash32()
         integer :: index2
-        integer(int32) :: hash
+        integer(int_hash) :: hash
         integer(int32) :: seed = 0_int32
         real :: t1, t2, tdiff
-        integer(int32) :: summary(repeat)
+        integer(int_hash) :: summary(repeat)
 
         call new_nmhash32_seed( seed )
         do k=1, size(block_size)
@@ -145,10 +142,10 @@ contains
 
     subroutine test_nmhash32x()
         integer :: index2
-        integer(int32) :: hash
+        integer(int_hash) :: hash
         integer(int32) :: seed = 0_int32
         real :: t1, t2, tdiff
-        integer(int32) :: summary(repeat)
+        integer(int_hash) :: summary(repeat)
 
         call new_nmhash32x_seed( seed )
         do k=1, size(block_size)
@@ -172,10 +169,10 @@ contains
 
     subroutine test_water()
         integer :: index2
-        integer(int32) :: hash
+        integer(int_hash) :: hash
         integer(int64) :: seed = 0_int64
         real :: t1, t2, tdiff
-        integer(int32) :: summary(repeat)
+        integer(int_hash) :: summary(repeat)
 
         call new_water_hash_seed( seed )
         do k=1, size(block_size)
